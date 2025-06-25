0636d44ce0aeef7bfdedda2fdca1e5746074c30c
Jediael Borges <jediaelborges23@gmail.com>
2025-06-25 20:03:27 -0300

    Environmet e Services preparados para requisição GET

--- src/app/core/services/schedules.service.ts
+++ src/app/core/services/schedules.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { Schedule } from '../types/types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SchedulesService {
 
-  constructor() { }
+  private readonly apiUrl: string = environment.apiUrl;
+
+  constructor( private http: HttpClient ) { }
+
+  listSchedules() {
+    return this.http.get<Schedule[]>(`${this.apiUrl}/agendamentos`);
+  }
 }
