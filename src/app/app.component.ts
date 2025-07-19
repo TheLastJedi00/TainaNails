deb78689c3ba76d0bd65d562beba1bdd58bf8bdd
Jediael Borges <jediaelborges23@gmail.com>
2025-07-19 18:56:09 -0300

    Novo design da página inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
 import { LoginComponent } from "./pages/login/login.component";
+import { HeaderComponent } from "./shared/header/header.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, LoginComponent],
+  imports: [RouterOutlet, HomeComponent, LoginComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,14 @@ import { SmartContainerComponent } from "../../shared/smart-container/smart-cont
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from "../../shared/header/header.component";
+import {MatListModule} from '@angular/material/list';
+import { FooterComponent } from "../../shared/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SmartContainerComponent, MatButtonModule, MatIconModule, RouterModule],
+  imports: [SmartContainerComponent, MatButtonModule, MatIconModule, RouterModule, HeaderComponent, MatListModule, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
