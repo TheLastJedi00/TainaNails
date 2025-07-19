ff592a0f8c9084f1bbd01168303260aec6a8ffcf
Jediael Borges <jediaelborges23@gmail.com>
2025-07-19 20:59:46 -0300

    Responsividade mobile

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [ MatButtonModule ],
+  imports: [ MatButtonModule, RouterModule ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
