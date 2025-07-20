f093d365fd461dce4d414888b952e2d8dc372b95
Jediael Borges <jediaelborges23@gmail.com>
2025-07-20 16:21:29 -0300

    Validação de não Domingo no campo data

---
+++ src/app/shared/dialog/dialog-utils/dialog-utils.ts
@@ -0,0 +1,4 @@
+export function dateValidator(date: string){
+    let isoDate = new Date(date).toISOString();
+    console.log(isoDate);
+}
--- src/app/shared/dialog/dialog.component.ts
+++ src/app/shared/dialog/dialog.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import {
   FormBuilder,
   Validators,
   FormsModule,
   ReactiveFormsModule,
+  AbstractControl,
+  ValidationErrors,
+  Form,
+  FormGroup,
 } from '@angular/forms';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
@@ -13,7 +17,11 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { CommonModule } from '@angular/common';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
+import {
+  MAT_DATE_LOCALE,
+  provideNativeDateAdapter,
+} from '@angular/material/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-dialog',
@@ -29,36 +37,25 @@ import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/cor
     MatSelectModule,
     CommonModule,
     MatButtonModule,
-    MatDatepickerModule
-],
-  providers: [ provideNativeDateAdapter(),
+    MatDatepickerModule,
+  ],
+  providers: [
+    provideNativeDateAdapter(),
     { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
-   ],
+    { provide: MAT_DIALOG_DATA, useValue: {} },
+  ],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss',
 })
 export class DialogComponent {
   services: { id: number; name: string }[];
   timeList!: number[];
-
-  firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required],
-  });
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
-  });
-  thirdFormGroup = this._formBuilder.group({
-   thirdCtrl: ['', Validators.required],
-  });
-  fourthFormGroup = this._formBuilder.group({
-    fourthCtrl: ['', Validators.required],
-  });
-  fifthFormGroup = this._formBuilder.group({
-    fifthCtrl: ['', Validators.required],
-  });
-
-  isLinear = false;
-
+  isLinear: boolean = false;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
+  fourthFormGroup: FormGroup;
+  fifthFormGroup: FormGroup;
 
   constructor(private _formBuilder: FormBuilder) {
     this.services = [
@@ -85,6 +82,37 @@ export class DialogComponent {
       { id: 21, name: 'Remoção de Alongamento' },
     ];
 
-    this.timeList = [8, 9, 10, 11, 14 ,15, 16, 17, 18]
+    this.timeList = [8, 9, 10, 11, 14, 15, 16, 17, 18];
+    
+    this.firstFormGroup = this._formBuilder.group({
+      nameCtrl: ['', Validators.required],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      phoneCtrl: ['', Validators.required],
+    });
+    this.thirdFormGroup = this._formBuilder.group({
+      serviceCtrl: ['', Validators.required],
+    });
+    this.fourthFormGroup = this._formBuilder.group({
+      dateCtrl: ['', [Validators.required], [this.dateValidator]],
+    });
+    this.fifthFormGroup = this._formBuilder.group({
+      timeCtrl: ['', Validators.required],
+    })
   }
+
+  dateValidator = (
+    control: AbstractControl
+  ): Observable<ValidationErrors | null> => {
+    const date = control.value as Date;
+    if (!date) {
+      return of(null);
+    }
+
+    if (date.getDay() === 0) {
+      return of({ sundayNotAvailable: true });
+    }
+
+    return of(null);
+  };
 }
