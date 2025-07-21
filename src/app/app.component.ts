b753e7588662a996af02606fd0b07cd257790b1c
Jediael Borges <jediaelborges23@gmail.com>
2025-07-21 17:31:02 -0300

    Criação da página de administrador em progresso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
 import { LoginComponent } from "./pages/login/login.component";
 import { HeaderComponent } from "./shared/header/header.component";
+import { AdminComponent } from "./pages/admin/admin.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, LoginComponent, HeaderComponent],
+  imports: [RouterOutlet, HomeComponent, LoginComponent, HeaderComponent, AdminComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
