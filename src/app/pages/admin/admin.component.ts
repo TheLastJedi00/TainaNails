b4852384b031e340d7a15d6d2a1fb845aba4c5aa
Jediael Borges <jediaelborges23@gmail.com>
2025-07-27 21:44:26 -0300

    Design de admin page

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,43 +1,41 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
+import { Component } from '@angular/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { provideNativeDateAdapter } from '@angular/material/core';
 import { RouterModule } from '@angular/router';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from "@angular/material/divider";
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
   imports: [
-    RouterModule,
-    MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
     MatDatepickerModule,
-    MatExpansionModule,
+    MatButtonModule,
+    RouterModule,
+    MatMenuModule,
     MatDividerModule,
-    MatListModule,
-    CommonModule,
+    CommonModule
+],
+  providers: [
   ],
-  providers: [provideNativeDateAdapter()],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss',
 })
 export class AdminComponent {
-  timeSlots: number[] = [];
-  @ViewChild(MatAccordion) accordion!: MatAccordion;
-
   normalDaySlots: number[] = [14, 15, 16, 17, 18];
-  wednesdaySlots: number[] = [ 15, 16, 17, 18];
-  saturdaySlots: number[] = [ 8, 9, 10, 11, 14, 15, 16, 17, 18];
-  panelOpenState = false;
+  wednesdaySlots: number[] = [15, 16, 17, 18];
+  saturdaySlots: number[] = [8, 9, 10, 11, 14, 15, 16, 17, 18];
+  weekdays: string[] = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado' ];
 
-  constructor() {}
+  slotIsAvailable(slot: string): string {
+    switch (slot) {}
+    return "accent";
+  }
 }
