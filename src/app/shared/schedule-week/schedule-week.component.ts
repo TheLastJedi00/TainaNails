c9976f620631025fb4fc8b8b7ab472ac91155e5f
Jediael Borges <jediaelborges23@gmail.com>
2025-08-07 18:02:16 -0300

    Dialogs de agendar e buscar por semana no painel do admin

--- src/app/shared/schedule-week/schedule-week.component.ts
+++ src/app/shared/schedule-week/schedule-week.component.ts
@@ -1,7 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatNativeDateModule } from '@angular/material/core';
+import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -18,10 +26,34 @@ import { MatInputModule } from '@angular/material/input';
     MatDatepickerModule,
     MatFormFieldModule,
     MatInputModule,
-    MatNativeDateModule,
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
+  providers: [
+    provideNativeDateAdapter(),
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
   ],
   templateUrl: './schedule-week.component.html',
   styleUrl: './schedule-week.component.scss',
 })
-export class ScheduleWeekComponent {}
+export class ScheduleWeekComponent {
+  dateControl = new FormControl(
+    '',
+    [Validators.required,
+    this.dateValidator]
+  );
+
+  dateValidator(control: AbstractControl): ValidationErrors | null {
+    if (!control.value) {
+      return null;
+    }
+
+    const date = new Date(control.value);
+
+    if (date.getDay() !== 1) {
+      return { invalidDate: true };
+    }
+    return null;
+  }
+}
