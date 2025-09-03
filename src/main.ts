dd2c0c360daebc879dcede12dd262a8c8292e2aa
Jediael Borges <jediaelborges23@gmail.com>
2025-09-03 16:34:55 -0300

    Erro caso aplicação busque api em LocalHost

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
+import { environment } from './environments/environment.development';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+  
+if (environment.production && environment.apiUrl.includes('localhost')) {
+  throw new Error('API URL não pode ser localhost em produção!');
+}
