ac508409f9211bcf4468d3c3197db9fa5383c308
Jediael Borges <jediaelborges23@gmail.com>
2025-09-15 21:02:11 -0300

    feature: componente pra confirmação de CRUD gerado

---
+++ src/app/shared/response-dialog/response-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-response-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './response-dialog.component.html',
+  styleUrl: './response-dialog.component.scss'
+})
+export class ResponseDialogComponent {
+
+}
