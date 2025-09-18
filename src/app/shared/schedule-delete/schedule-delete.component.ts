8b7d582e8f086ad600c0548b479a6b6224ae2864
Jediael Borges <jediaelborges23@gmail.com>
2025-09-18 14:54:52 -0300

    feature: loading ao excluir agendamentos

--- src/app/shared/schedule-delete/schedule-delete.component.ts
+++ src/app/shared/schedule-delete/schedule-delete.component.ts
@@ -1,9 +1,17 @@
 import { Component, Inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from "@angular/material/dialog";
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { SchedulesService } from '../../core/services/schedules.service';
 import { ScheduleInfoComponent } from '../schedule-info/schedule-info.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { CommonModule } from '@angular/common';
+import { ResponseDialogComponent } from '../response-dialog/response-dialog.component';
 
 @Component({
   selector: 'app-schedule-delete',
@@ -11,10 +19,12 @@ import { ScheduleInfoComponent } from '../schedule-info/schedule-info.component'
   imports: [
     MatButtonModule,
     MatIconModule,
-    MatDialogModule
-],
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    CommonModule,
+  ],
   templateUrl: './schedule-delete.component.html',
-  styleUrl: './schedule-delete.component.scss'
+  styleUrl: './schedule-delete.component.scss',
 })
 export class ScheduleDeleteComponent {
   idValue: string = this.data.id;
@@ -22,26 +32,61 @@ export class ScheduleDeleteComponent {
   phoneValue: string = this.data.phone;
   serviceValue: string = this.data.service;
   dateValue: string = this.data.date;
+  isLoading: boolean = false;
 
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      id: string;
+      name: string;
+      phone: string;
+      service: string;
+      date: string;
+    },
+    @Inject(SchedulesService) private schedulesService: SchedulesService,
+    public infoDialog: MatDialogRef<ScheduleInfoComponent>,
+    public responseDialog: MatDialog
+  ) {}
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: {
-        id: string,
-        name: string,
-        phone: string,
-        service: string,
-        date: string,
+  openResponseDialog(
+    isLoading: boolean,
+    matIcon?: string,
+    title?: string,
+    iconColor?: string,
+    description?: string,
+    strong?: string
+  ) {
+    const dialogRef = this.responseDialog.open(ResponseDialogComponent, {
+      data: {
+        isLoading: isLoading,
+        matIcon: matIcon,
+        title: title,
+        iconColor: iconColor,
+        description: description,
+        strong: strong,
       },
-      @Inject(SchedulesService) private schedulesService: SchedulesService,
-      public infoDialog: MatDialogRef<ScheduleInfoComponent>
-    ) {}
-
-  deleteSchedule() {
-    this.schedulesService.deleteSchedule(this.idValue).then(() => {
-      this.infoDialog.close();
-      console.log('Schedule deleted successfully');
-      window.location.reload();
-    }).catch((error) => {
-      console.error('Error deleting schedule:', error);
     });
+  }
+  deleteSchedule() {
+    this.isLoading = true;
+    this.schedulesService
+
+    const response = this.responseDialog.open(ResponseDialogComponent, {
+      data: { isLoading: true },
+    });
+
+    this.schedulesService
+      .deleteSchedule(this.idValue)
+      .then(() => {
+        response.close();
+        this.infoDialog.close();
+        window.location.reload();
+        this.openResponseDialog(false, 'check_circle', 'Agendamento cancelado com sucesso!', 'rgb(127, 206, 145)');
+      })
+      .catch((error) => {
+        response.close();
+        this.openResponseDialog(false, 'error', 'Infelizmente houve um erro', 'rgb(206, 127, 127)');
+        console.error('Error deleting schedule:', error);
+      });
   }
 }
